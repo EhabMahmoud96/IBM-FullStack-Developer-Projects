8093c7fd2a7f2129c9e800cf2aa2d87baa984996
EhabMahmoud96 <ehabmahmoudlionX96@yahoo.com>
2025-02-19 07:59:30 -0500

    solving the problem that the the area didnt produce in the p tag because the let is function block scope not global

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -4,11 +4,9 @@ let width;
 //create a function called calculateArea
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value; 
+    width = parseFloat(document.getElementById('width').value);
 
     // calculate area 
     let area = length * width;
+    document.getElementById('result').innerText = `The area of the ractangle is: ${area}`;
 }
-
-//updating the value inside the p tag
-document.getElementById('result').innerText = `The area of the ractangle is: $(area)`;
