62784811d85532a1be0319b09a8c644f30f74c83
EhabMahmoud96 <ehabmahmoudlionX96@yahoo.com>
2025-02-23 04:13:40 -0500

    update and fix to the program file where findEmployeeById(2) wasn't working because of join() which is intended to be used in strings and find returns an array and made a new button 'findEmployeeSpecializedInJavaScript()'

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000,Specialization: 'JavaScript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000,Specialization: 'Python'},
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 ,Specialization: 'Java'},
       //... More employee records can be added here
     ];
 
@@ -31,10 +31,21 @@ function displayHREmployees() {
 function findEmployeeById(employeeId) {
     const foundEmployee = employees.find(employee => employee.id === employeeId);
     if (foundEmployee) {
-        document.getElementById('emplloyeeDetails').innerHTML = (`<p>${foundEmployee.id}: ${foundEmployee.name}: $${foundEmployee.salary}- ${foundEmployee.age}- ${foundEmployee.department}</p>`).join('');
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: $${foundEmployee.salary}- ${foundEmployee.age}- ${foundEmployee.department}</p>`;
     }
     else{
         //if the id isn't found in the employees database (empployees array)
         document.getElementById('employeesDetails').innerHTML = 'No employee has been found with this ID';
+    }
+}
+
+
+//Creating a function to find by JavaScript Specialized Employee
+function findEmployeeSpecializedInJavaScript(){
+    const EmployeesSpecializedInJavaScript = employees.find( employee => employee.Specialization === 'JavaScript');
+    if (EmployeesSpecializedInJavaScript) {
+        document.getElementById('employeesDetails').innerHTML = `<p> Id :${EmployeesSpecializedInJavaScript.id}- Name: ${EmployeesSpecializedInJavaScript.name}- Salary: $${EmployeesSpecializedInJavaScript.salary} - Age: ${EmployeesSpecializedInJavaScript.age}- Department: ${EmployeesSpecializedInJavaScript.department} </p>`;
+    } else{
+        document.getElementById('employeesDetails').innerHTML = `No Specialized JavaScript Developer is Found`;
     }
 }
