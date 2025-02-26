eeb8d0d9ae962570bb13faaa9f8e5a626f347d8d
EhabMahmoud96 <ehabmahmoudlionX96@yahoo.com>
2025-02-26 04:25:32 -0500

    Updated the web app to include multiplication ,devison ,and addition similtiuonisly

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,11 +1,12 @@
 function performOperation() {
+    debugger;
     // Get the user input from fields
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
         //perform the operation
-            let result = add(num1, num2);
+            let result = multipliy_add_divide(num1, num2);
 
             //display The result
             displayResult(result);
@@ -14,12 +15,14 @@ function performOperation() {
      }
     }
 
-    function add(a, b) {
+    function multipliy_add_divide(a, b) {
         //Introduction a debugger statement to pause execution
-        debugger;
 
         //Multibly the numbers 
-        return a + b;
+        let x = a + b;
+        let y = x * a;
+        let z = Math.ceil(x / y);
+        return z;
     }
 
     function displayResult(result) {
